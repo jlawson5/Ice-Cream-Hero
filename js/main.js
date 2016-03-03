289f0aadab77e4627a1a3e8beb5bc0636af96009
jlawson5 <jlawson5@masonlive.gmu.edu>
2016-03-03 11:45:53 -0500

    changed player sprite, a lot of code
    Coded just about everything, still needs some minor fixes

--- js/main.js
+++ js/main.js
@@ -6,33 +6,32 @@
 
     window.onload = function() {
 
-        var game = new Phaser.Game(600, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+        var game = new Phaser.Game(500, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
-    game.load.image('platform', 'assets/GravityManPlatform.png');//Floor and Platform objects are not working properly//
-    game.load.image('floor', 'assets/GravityManFloor.png');
-    game.load.spritesheet('gravityMan', 'assets/GravityMan.png', 32, 32);//Gravity Man: The player character//
-    game.load.image('coin', 'assets/GravityManCoin.png');//Coins the player must find and collect//
-    game.load.image('background', 'assets/GravityManBG.png');//Background image//
-    game.load.audio('coinCollect', 'assets/Electronic_Chime-KevanGC-495939803.mp3');
-    game.load.audio('gravityChange', 'assets/spin_jump-Brandino480-2020916281.mp3');
+    game.load.image('bg', 'assets/bg.png');//background//
+    game.load.image('hero', 'assets/Hero.png');//our hero, determined to get the ice cream//
+    game.load.image('icecream', 'assets/IceCream.png');//Ice Cream: Collect these to increase your time by 3 and your score by 1000//
+    game.load.image('spinach', 'assets/GrossSpinach.png');//Cans of Spinach: The disgusting greens your parents keep saying is good for you. Tastes disgusting and reduces time by 10; but it's good for you so it gives you 10000 points//
 }
 
+var isRunning = false;//if the game is running//
 var player;
-var facing = 'left';
-var jumpTimer = 0;
 var cursors;
-var jumpButton;
-var bg;
-var g_dir = 'down';//current direction of gravity//
-var coinSound;
-var gSound;
-var coins;
-var score = 0;
-var scoreString = '';
+var bg1;//two instances are used for wrapping
+var bg2;
+var score = 0;//increments each in-game step. Collecting ice cream and spinach can quickly earn you more points//
+var scoreString = 'Score: ';
 var scoreText;
-var floor;//Not working//
+var timeLeft = 60;//time remaining in seconds, -1 about every 1000 in-game steps. Gameover when time = 0//
+var timeTimer = 1000;//a bit of a stupidly named variable; keeps track of the remaining time limit using the in-game timer//
+var timeString = 'Time: ';
+var timeText;
+var icecream;//ice cream group//
+var spinach;//gross spinach group//
+var itemTimer = 1000;//timer on which spinach and icecream spawn//
+var stateText;
 
 function create() {
 
@@ -40,161 +39,162 @@ function create() {
 
     game.stage.backgroundColor = '#000000';
 
-    bg = game.add.tileSprite(0, 0, 800, 600, 'background');
-    bg.fixedToCamera = true;
+    bg1 = game.add.sprite(0, 0, 'bg');
+    game.physics.enable(bg1, Phaser.Physics.ARCADE);
+    bg1.body.setSize(500, 600, 0, 0);
+    bg2 = game.add.sprite(0, 600, 'bg');
+    game.physics.enable(bg2, Phaser.Physics.ARCADE);
+    bg2.body.setSize(500, 600, 0, 0);
 
-    game.physics.arcade.gravity.y = 250;
-    
-    //adds sounds to the game//
-    coinSound = game.add.audio('coinCollect');
-    gSound = game.add.audio('gravityChange');
-    coinSound.allowMultiple = true;
-    gSound.allowMultiple = true;
-    coinSound.addMarker('coinGet', 0, 2.0);
-    gSound.addMarker('gSwitch', 0, 0.5);
-
-    player = game.add.sprite(32, 32, 'gravityMan');
+    player = game.add.sprite(250, 400, 'hero');
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
-    player.body.bounce.y = 0.2;
     player.body.collideWorldBounds = true;
-    player.body.setSize(16, 32, 8, 0);
-
-    player.animations.add('left', [2, 3], 10, true);
-    player.animations.add('right', [0, 1], 10, true);
-    player.animations.add('left_up', [6, 7], 10, true);//player is facing left and gravity is up//
-    player.animations.add('right_up', [4, 5], 10, true);//player is facing right and gravity is up//
+    player.body.setSize(64, 64, 0, 0);
     
-    coins = game.add.group();
-    createCoins();
+    icecream = game.add.group();
+    spinach = game.add.group();
     
-    scoreString = 'Score: ';
     scoreText = game.add.text(10, 10, scoreString + score, {font: '34px Arial', fill: '#fff'});
-
-    game.camera.follow(player);
+    timeText = game.add.text(10, 50, timeString + timeLeft, {font: '34px Arial', fill: '#fff'});
+    stateText = game.add.text(130, 300, "Click to play!", {font: '34px Arial', fill: '#000'});
 
     cursors = game.input.keyboard.createCursorKeys();
-    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
 }
 
 function update() {
-
-    game.physics.arcade.collide(player, floor);
-
-    player.body.velocity.x = 0;
-
-    if (cursors.left.isDown)
+    if(isRunning)
     {
-        player.body.velocity.x = -150;
-
-        //if (facing != 'left')
-        //{
-            if(g_dir == 'down')
-                player.animations.play('left');
-            else
-                player.animations.play('left_up');
-            facing = 'left';
-        //}
-    }
-    else if (cursors.right.isDown)
-    {
-        player.body.velocity.x = 150;
-
-        //if (facing != 'right')
-        //{
-            if(g_dir == 'down')
-                player.animations.play('right');
-            else
-                player.animations.play('right_up');
-            facing = 'right';
-        //}
-    }
-    else
-    {
-        //if (facing != 'idle')
-        //{
-            player.animations.stop();
-
-            if (facing == 'left')
-            {
-                if(g_dir == 'down')
-                    player.frame = 2;
-                else
-                    player.frame = 6;
-            }
-            else if(facing == 'right')
-            {
-                if(g_dir == 'down')
-                    player.frame = 0;
-                else
-                    player.frame = 4;
-            }
-
-            facing = 'idle';
-        //}
-    }
-    
-    if (jumpButton.isDown && game.time.now > jumpTimer)
-    {
-        //player.body.velocity.y = -250;
-        game.physics.arcade.gravity.y *= -1;
-        if (g_dir == 'down')//if gravity is acting downwards, set gravity to 'up'//
+        score++;
+        bg1.body.velocity.y = 300;
+        bg2.body.velocity.y = 300;
+        if (cursors.left.isDown)
         {
-            g_dir = 'up';
+            player.body.velocity.x = -200;
+            player.body.velocity.y = 0;
+        }
+        else if (cursors.right.isDown)
+        {
+            player.body.velocity.x = 200;
+            player.body.velocity.y = 0;
+        }
+        else if (cursors.up.isDown)
+        {
+            player.body.velocity.y = -200;
+            player.body.velocity.x = 0;
+        }
+        else if (cursors.down.isDown)
+        {
+            player.body.velocity.y = 200;
+            player.body.velocity.x = 0;
         }
         else
         {
-            g_dir = 'down';
+            player.body.velocity.x = 0;
+            player.body.velocity.y = 0;
         }
-        gSound.play('gSwitch');
-        jumpTimer = game.time.now + 750;
+        
+        //itemTimer and timeTimer run on the same time, but I have them seperate to be safe//
+        if(game.time.now > itemTimer && timeLeft > 0)
+        {
+            spawnItem();
+            itemTimer = game.time.now + 1000;
+        }
+        
+        if(game.time.now > timeTimer && timeLeft > 0)
+        {
+            timeLeft--;
+            timeTimer = game.time.now + 1000;
+        }
     }
-    
-    //Check for and handles coin collisions//
-    game.physics.arcade.overlap(player, coins, coinCollision, null, this);
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//function createCoins(): spawns 15 coins to random coordinates with random bounce (coins will share the players gravity).  //
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function createCoins()
-{
-    for(var i = 0; i < 15; i++)
+    else
     {
-        var coin = coins.create((Math.random() * 600) + 1, 16, 'coin');
-        game.physics.enable(coin, Phaser.Physics.ARCADE);
-        coin.body.bounce.y = 1;
-        coin.body.setSize(16, 16, 0, 0);
-        coin.body.collideWorldBounds = true;
+        player.body.velocity.x = 0;
+        player.body.velocity.y = 0;
+        game.input.onTap.addOnce(startGame,this);
+        bg1.body.velocity.y = 0;
+        bg2.body.velocity.y = 0;
     }
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-//function coinCollision(coin, player)                                                                 //
-//destroys the coin and adds 100 to the score. Spawns another coin with random bounce and x-coordinates//
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-function coinCollision(player, coin)
-{
-    coin.kill();
-    
-    coinSound.play('coinGet');
-    score += 100;
+    timeText.text = timeString + timeLeft;
     scoreText.text = scoreString + score;
     
-    var newCoin = coins.create((Math.random() * 600) + 1, 16, 'coin');
-    game.physics.enable(newCoin, Phaser.Physics.ARCADE);
-    newCoin.body.bounce.y = 1;
-    newCoin.body.setSize(16, 16, 0, 0);
-    newCoin.body.collideWorldBounds = true;
+    if(bg1.body.y == 600)
+        bg1.body.y = -600;
+    
+    if(bg2.body.y == 600)
+        bg2.body.y = -600;
+    
+    if(timeLeft <= 0)
+    {
+        isRunning = false;
+        icecream.removeAll(true);
+        spinach.removeAll(true);
+        stateText.visible = true;
+        game.input.onTap.addOnce(startGame,this);
+    }
+    
+    //collisions//
+    game.physics.arcade.overlap(player, icecream, iceCreamCollision, null, this);
+    game.physics.arcade.overlap(player, spinach, spinachCollision, null, this);
+}
+        
+function spawnItem()
+{
+    var roll = Math.random();
+    var coord = Math.random() * 3;//used to determine which of three possible x-coordinates the item will spawn//
+    var xCoord;//actual x-coordinate item will spawn at//
+    if(coord < 1)
+        xCoord = 125;
+    else if(coord < 2)
+        xCoord = 250;
+    else
+        xCoord = 375;
+    if(roll <= .49)//ice cream, yay!//
+    {
+        var cone = icecream.create(xCoord, -32, 'icecream');
+        game.physics.enable(cone, Phaser.Physics.ARCADE);
+        cone.body.setSize(24, 24, 0, 0);
+        cone.body.velocity.y = 300;
+    }
+    
+    else//bleh, gross spinach//
+    {
+        var can = spinach.create(xCoord, -32, 'spinach');
+        game.physics.enable(can, Phaser.Physics.ARCADE);
+        can.body.setSize(24, 24, 0, 0);
+        can.body.velocity.y = 300;
+    }
+}
+        
+function startGame()
+{
+    isRunning = true;
+    timeLeft = 60;
+    score = 0;
+    player.body.x = 250;
+    player.body.y = 400;
+    timeTimer = game.time.now + 1000;
+    itemTimer = game.time.now + 1000;
+    stateText.text = "   Gameover! \nClick to play again!"
+    stateText.visible = false;
+}
+        
+function iceCreamCollision(player, icecream)
+{
+    icecream.kill();
+    score += 1000;
+    timeLeft += 3;
+}
+        
+function spinachCollision(player, spinach)
+{
+    spinach.kill();
+    score += 10000;//a lot of points for a lot of gross. Is it worth it?//
+    timeLeft -= 10;
+    if(timeLeft < 0)
+        timeLeft = 0;
 }
 
-function render () {
-
-    // game.debug.text(game.time.physicsElapsed, 32, 32);
-    // game.debug.body(player);
-    // game.debug.bodyInfo(player, 16, 24);
-
-}
+function render () {}
 
     };
